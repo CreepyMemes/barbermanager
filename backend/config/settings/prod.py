697d3600714cdcf02c6da856cdcf01612c37b43c
s1lentCommit <61922260+s1lentCommit@users.noreply.github.com>
2025-05-21 00:09:32 +0200

    disabled django browser crap, must repond only in json

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -2,3 +2,6 @@ from .base import *
 
 DEBUG = False
 FRONTEND_URL = 'http://manageBarber.s1lentCommit.org'
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+)
