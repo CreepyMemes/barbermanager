920b51081a9e8ff60b876d9526da327395825f9c
s1lentCommit <61922260+s1lentCommit@users.noreply.github.com>
2025-06-28 13:49:18 +0200

    added icons

--- frontend/assets/icons/index.js
+++ frontend/assets/icons/index.js
@@ -1,9 +1,13 @@
 import manageBarber from './manageBarber.svg';
 import github from './github.svg';
+import dashboard from './dashboard.svg';
+import appointment from './appointment.svg';
 
 const icons = {
   manageBarber,
   github,
+  dashboard,
+  appointment,
 };
 
 export default icons;
