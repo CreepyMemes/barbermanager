1ba4e3413fc3febd1faaa8d49bbe4fe079114e37
s1lentCommit <61922260+s1lentCommit@users.noreply.github.com>
2025-07-26 16:31:32 +0200

    - changed max line widh to 130 in prettier config
    - updated files that gave problems due to this change

--- frontend/src/components/layout/Footer/Footer.jsx
+++ frontend/src/components/layout/Footer/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
               className={styles.button}
               href="https://github.com/s1lentCommit/manageBarber"
               size="md"
-              color="animated"
+              color="animated" //
             >
               <Icon name="github" size={'md'} />
             </Button>
